fb8cd154d1cf523d2054221ec50b8082674f49a9
easec <mats.johannesson@easec.se>
2024-05-17 21:34:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
-sudo usermod –aG docker $USER
+sudo usermod -aG docker $USER
 
